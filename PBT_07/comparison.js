9dc600c3432d016f4b276254d304682b21bdfda9
qkhai2512 <quangkhai251224@gmail.com>
2026-05-31 10:52:38 +0700

    [FEATURE] Add equality comparison exercises

---
+++ PBT_07/comparison.js
@@ -0,0 +1,15 @@
+console.log('5 == "5" :', 5 == "5");
+
+console.log('5 === "5" :', 5 === "5");
+
+console.log("null == undefined :", null == undefined);
+
+console.log("null === undefined :", null === undefined);
+
+console.log("NaN == NaN :", NaN == NaN);
+
+console.log("0 == false :", 0 == false);
+
+console.log("0 === false :", 0 === false);
+
+console.log('"" == false :', "" == false);
