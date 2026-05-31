609d756d9f2e931473f0f9bbddff6b953b3b26bf
qkhai2512 <quangkhai251224@gmail.com>
2026-05-31 10:54:21 +0700

    [FEATURE] Add template literals examples

---
+++ PBT_07/template_literals.js
@@ -0,0 +1,57 @@
+const name = "Nam";
+const age = 20;
+
+const greeting =
+    `Xin chào ${name}! Bạn ${age} tuổi.`;
+
+console.log(greeting);
+
+const userId = 1001;
+const page = 2;
+
+const url =
+    `https://api.example.com/users/${userId}/orders?page=${page}`;
+
+console.log(url);
+
+const title = "iPhone 16";
+const description = "Điện thoại Apple";
+const price = 25990000;
+
+const html = `
+<div class="card">
+    <h2>${title}</h2>
+    <p>${description}</p>
+    <span>Giá: ${price}đ</span>
+</div>
+`;
+
+console.log(html);const name = "Nam";
+const age = 20;
+
+const greeting =
+    `Xin chào ${name}! Bạn ${age} tuổi.`;
+
+console.log(greeting);
+
+const userId = 1001;
+const page = 2;
+
+const url =
+    `https://api.example.com/users/${userId}/orders?page=${page}`;
+
+console.log(url);
+
+const title = "iPhone 16";
+const description = "Điện thoại Apple";
+const price = 25990000;
+
+const html = `
+<div class="card">
+    <h2>${title}</h2>
+    <p>${description}</p>
+    <span>Giá: ${price}đ</span>
+</div>
+`;
+
+console.log(html);
