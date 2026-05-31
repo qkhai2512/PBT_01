433699b2658ae20009585fbc7baec4b91963cd6c
qkhai2512 <quangkhai251224@gmail.com>
2026-05-31 10:51:47 +0700

    [FEATURE] Add data types and coercion examples

---
+++ PBT_07/data_types.js
@@ -0,0 +1,19 @@
+console.log("typeof null =", typeof null);
+
+console.log("typeof undefined =", typeof undefined);
+
+console.log("typeof NaN =", typeof NaN);
+
+console.log('"5" + 3 =', "5" + 3);
+
+console.log('"5" - 3 =', "5" - 3);
+
+console.log('"5" * "3" =', "5" * "3");
+
+console.log("true + true =", true + true);
+
+console.log("[] + [] =", [] + []);
+
+console.log("[] + {} =", [] + {});
+
+console.log("{} + [] =", {} + []);
