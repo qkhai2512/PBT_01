1151bc0725f7fe7bc106e4ddefe74e3cab492148
qkhai2512 <quangkhai251224@gmail.com>
2026-05-31 10:53:14 +0700

    [FEATURE] Add truthy falsy demonstrations

---
+++ PBT_07/truthy_falsy.js
@@ -0,0 +1,31 @@
+if ("0") {
+    console.log("A");
+}
+
+if ("") {
+    console.log("B");
+}
+
+if ([]) {
+    console.log("C");
+}
+
+if ({}) {
+    console.log("D");
+}
+
+if (null) {
+    console.log("E");
+}
+
+if (0) {
+    console.log("F");
+}
+
+if (-1) {
+    console.log("G");
+}
+
+if (" ") {
+    console.log("H");
+}
